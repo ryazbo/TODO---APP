35ff655e0f5d1c13dc4d978a5234c1433ac27f7f
Deiby S <136501261+Deiby101@users.noreply.github.com>
2024-09-09 20:38:16 -0500

    abc

--- ReactApp1/ReactApp1.Server/Models/TodoappContext.cs
+++ ReactApp1/ReactApp1.Server/Models/TodoappContext.cs
@@ -19,7 +19,7 @@ public partial class TodoappContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=192.168.1.3;Database=todoapp;User ID=sa;Password=Juan310*;Trusted_Connection=False;Encrypt=false;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-ATQUJGQ\\DEYY;Database=todoapp;User ID=sa;Password=Xdlolsalu302052020;Trusted_Connection=False;Encrypt=false;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
